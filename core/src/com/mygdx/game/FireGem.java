0289de08de9d15f0ae60ce2e91e351340084054a
camet2651 <camet2651@HRH-IC0027336.wrdsb.ca>
2018-12-06 09:00:05 -0500

    fixed drawing

---
+++ core/src/com/mygdx/game/FireGem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+/**
+ *
+ * @author biGgEsT yEeT: tHe fiNaL fOrM
+ */
+public class FireGem extends Gem{
+    /**
+     * initalizes fire gem as subclass of gem with "fireboy" as char allowed
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param width the width
+     * @param height the height
+     */
+    public FireGem(float x, float y, int width, int height) {
+        super(x, y);
+    } 
+}
