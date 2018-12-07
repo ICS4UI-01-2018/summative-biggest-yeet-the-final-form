90cbc1f22a3b3aeca39e0134186ba029d144c394
camet2651 <camet2651@HRH-IC0027336.wrdsb.ca>
2018-12-07 08:20:25 -0500

    jumping in game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -119,8 +119,14 @@ public class MyGdxGame extends ApplicationAdapter {
         // constantly update the x and y positions of the Fireboy and the Watergirl
         fireboy.updatePostions();
         watergirl.updatePostions();
-          this.platforms[0].collision(fireboy);
-        fireboy.falling(this.platforms[0]);
+        for (int i = 0; i < platforms.length; i++) {
+            this.platforms[i].collision(fireboy);
+            fireboy.falling(this.platforms[i]);
+        }
+        
+        
+        this.platforms[0].collision(watergirl);
+        watergirl.falling(this.platforms[0]);
         // Fireboy keyboard listeners
         // make the Fireboy move left
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
