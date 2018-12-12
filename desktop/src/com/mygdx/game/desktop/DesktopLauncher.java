0c2440b64f9a4f982888a503431038cb7ba74521
hadim9637 <hadim9637@HRH-IC0027334.wrdsb.ca>
2018-12-12 08:45:33 -0500

    Merge origin/master
    Conflicts:
    	core/src/com/mygdx/game/Platform.java

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.testFalling;
-
+import com.mygdx.game.LevelTwo;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//                new LwjglApplication(new MyGdxGame(), config);
-         new LwjglApplication(new testFalling(), config);
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                new LwjglApplication(new MyGdxGame(), config);
+//        new LwjglApplication(new testFalling(), config);
+//        new LwjglApplication(new LevelTwo(), config);
+    }
 }
