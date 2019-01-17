bcc8f30fdf075dfc8e5b8d4e0cdf2af049f40bb2
hadim9637 <hadim9637@HRH-IC0027334.wrdsb.ca>
2019-01-17 08:17:51 -0500

    Started coding level 2

--- core/src/com/mygdx/game/LevelTwo.java
+++ core/src/com/mygdx/game/LevelTwo.java
@@ -17,40 +17,17 @@ public class LevelTwo extends Level {
      */
     @Override
     public void create() {
-        // initialize SpriteBatch, ShapeRenderer, OrthographicCamera, FitViewport
-        super.create();
-
         // initialize the Characters
-        super.fireboy = new Fireboy(22.5f, 28);
-        super.watergirl = new Watergirl(18, 28);
-
+        this.fireboy = new Fireboy(38.5,);
+        this.watergirl = new Watergirl();
+        
         // initialize the Platforms
         
-        // initialize the MovingPlatforms
-        super.movingPlatforms = new MovingPlatform[6];
+        // initialize the moving Platforms
         
         // initialize the Obstacles
-        super.fire = new Fire[1];
-        super.fire[0] = new Fire(1, 11, 7, 1);
-        super.water = new Water[1];
-        super.water[0] = new Water(34, 11, 7, 1);
-        super.mud = new Mud[1];
-        super.mud[0] = new Mud(1, 2, 40, 1);
         
-        // initialize the Gems
         // initialize the Doors
     }
 
-    /**
-     * Implement the basic game logic and draw all the game objects on the
-     * screen.
-     */
-    @Override
-    public void render() {
-        // clear the screen and implement the basic game logic
-        super.render();
-
-        // draw the game elements
-        super.draw();
-    }
 }
